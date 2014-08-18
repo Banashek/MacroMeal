892216473695e291088cd5035b0966ed50f8905a
Jonathan Banashek <jbanashek@gmail.com>
2014-08-18 14:04:52 -0700

    Removing recipe_type, cuisine, and recipe_preference from recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,5 @@
 class Recipe < ActiveRecord::Base
   DIFFICULTY=%w(Easy Medium Hard)
-  TYPE=%w(Breakfast BBQ Protien\ Powder)
-  PREFERENCE=%w(Gluten\ Free Dairy\ Free Low\ Fat High\ Protein)
-  CUISINE=%w(Asian Mediterranean Italian)
 
   def name=(value)
     write_attribute(:name, value.downcase)
